b3068fc3168f755d19097f74500e0f3d67099a28
jamestran1 <quang.tran@episerver.com>
2024-01-29 16:07:18 +0700

    fix env

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     experimental: {
         missingSuspenseWithCSRBailout: false,
       },
+      env: {
+        CMS_SAAS: process.env.CMS_SAAS,
+        OPTI_AI_KEY: process.env.OPTI_AI_KEY,
+        OPTI_GRAPH_KEY: process.env.OPTI_GRAPH_KEY,
+      }
 };
 
 export default nextConfig;
